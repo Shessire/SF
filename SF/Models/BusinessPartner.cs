f68032bebaf4a0a5ac0d4220fc8abbd9bbbb307a
wisuwat <wisuwatpond01@gmail.com>
2024-11-28 11:38:21 +0700

    Create Address and BP Model

---
+++ SF/Models/BusinessPartner.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace SF.Models
+{
+    public class BusinessPartner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // "Customer" or "Supplier"
+        public string EntityType { get; set; } // "Corporate" or "Individual"
+        public ICollection<Address> Addresses { get; set; }
+    }
+}
